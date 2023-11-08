7899ac8d44d482ff8a9c9f68918d53b678c52aef
Ozziel Yaxcal Leal <yaxcalozziel@gmail.com>
2023-11-07 19:11:48 -0600

    ok

--- Sistema/settings.py
+++ Sistema/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'Sistema.wsgi.application'
 
 DATABASES = {
     'default': {
-
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.',
         'NAME': BASE_DIR / 'db.sqlite3',
-
-
     }
 }
 
@@ -120,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
